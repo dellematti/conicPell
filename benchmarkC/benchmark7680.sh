134cba5069d0fcb68ac569522f1f67f2e861b4fa
dellematti <dellematti@hotmail.it>
2024-11-15 14:21:50 +0100

    update

--- benchmarkC/benchmark7680.sh
+++ benchmarkC/benchmark7680.sh
@@ -3,12 +3,11 @@
 # quelli solo di enc e dec ha senso farli con le stesse chiavi fisse su python e qui
 # quello di gen sarà invece chiaramente casuale e basta
 
-# questi sono benchmark a 512 bit, potrei aggiungerne altri nello stesso file o crearne altri diversi
-
+# questi sono benchmark a 7680 bit,
 
 
 # da terminale per far andare il file :
-# bash benchmark.sh     
+# bash benchmark7680.sh     
 
 
 #!/bin/bash
@@ -16,7 +15,7 @@
 # declare -i time=0
 declare -i sum=0
 # declare -i n=10000      
-declare -i n=3
+declare -i n=100
 
 
 for ((i=0; i<n; i++))    
@@ -28,20 +27,29 @@ do
 
     secondi=${time%.*} 
     millisecondi=${time#*.}
-    millisecondi=$(echo $millisecondi | cut -c 4-6)
+    millisecondi=$(echo $millisecondi | cut -c 1-3)
     
     tmp=$(echo $secondi 1000 | awk '{print $1*$2}')
-    sum=$(($tmp+$millisecondi + $sum))
+    tmp=$(echo $tmp $millisecondi | awk '{print $1+$2}')
+    # echo $millisecondi
+    sum+=$(($tmp))
 
-    # echo $time            # il tempo è in millisecondi
+    # echo $tmp            # il tempo è in millisecondi
+    # echo $sum    
 done
 # echo $time
 # il risultato ora è in millesimi di secondo
 
 
+
+
 media=$(echo "$sum $n" | awk '{printf "%.2f \n", $1/$2}') # media=$(($time/n))
+# media=$(echo "$sum $n" | awk '{print $1/$2}') # media=$(($time/n))
+
+media=$(echo $media| cut -c 1-4)
 
 
+echo $media "media"
 
 
 
@@ -52,26 +60,31 @@ declare -i sum=0
 for ((i=0; i<n; i++))    
 do
     time=$((./benchmark.out) 2>&1)
-    # time=$((./benchmarkGen.out) 2>&1)
-    # time=$((./benchmarkEnc.out) 2>&1)
-    # time=$((./benchmarkDec.out) 2>&1)
-
-    # SUBSTRING=$(echo $VAR| cut -c 4-5)
-
+    # VAR=$((./benchmarkGen.out) 2>&1)
+    # VAR=$((./benchmarkEnc.out) 2>&1)
+    # VAR=$((./benchmarkDec.out) 2>&1)
 
     secondi=${time%.*} 
     millisecondi=${time#*.}
-    millisecondi=$(echo $millisecondi | cut -c 4-6)
+    millisecondi=$(echo $millisecondi | cut -c 1-3)
     
     tmp=$(echo $secondi 1000 | awk '{print $1*$2}')
-    time=$(($tmp+$millisecondi))
+    tmp=$(echo $tmp $millisecondi | awk '{print $1+$2}')
+   
+    # echo "tempo"
+    # echo $tmp            # il tempo è in millisecondi
 
 
-    tmp=$(echo "$time $media" | awk '{print $1-$2}')
-    echo $tmp
-    tmp=$(echo $tmp $tmp | awk '{printf "%4.3f\n",$1*$2}')
+    tmp=$(echo "$tmp $media" | awk '{print $1-$2}')
 
-    sum=$(echo "$sum $tmp" | tr . , | awk '{print $1+$2}')    # tr perché dovevo scambiare "." e ","
+    # echo $tmp
+    #tmp=$(echo $tmp $tmp | awk '{printf "%4.3f\n",$1*$2}')
+    tmp=$(echo "$tmp $tmp" | awk '{print $1*$2}')
+
+
+    sum+=$(($tmp))
+
+    # sum=$(echo "$sum $tmp" | tr . , | awk '{print $1+$2}')    # tr perché dovevo scambiare "." e ","
     # echo $i  
 done
 
@@ -93,6 +106,33 @@ printf "\nnumero iterazioni = "$n"\n\n"
 # le varianze sono state ottenute dividendo per n ,non per n-1
 
 
-###############################################à valori a 512 bit
+###############################################à valori a 7680 bit
 
 
+# media = 4965 millesimi di secondo
+# varianza = 1179174.97
+# deviazione starndard = 1085.9
+# numero iterazioni = 100
+
+
+# real	16m42.291s
+# user	16m17.860s
+# sys	0m24.481s
+
+
+
+
+
+
+
+
+
+# media = 4970 millesimi di secondo
+# varianza = 1419012.02
+# deviazione starndard = 1191.22
+# numero iterazioni = 100
+
+
+# real	16m46.296s
+# user	16m20.962s
+# sys	0m25.424s
--- plot/plotIstanza.py
+++ plot/plotIstanza.py
@@ -3,13 +3,19 @@ import matplotlib.pyplot as plt
   
 X = ['Media','Deviazione'] 
 # valori a 512 bit
-Ygirls = [7.59,2.32] 
-Zboys = [33.63,9.79] 
+# c = [7.59,2.32] 
+# python = [33.63,9.79] 
+
+# valori a 7680 bit
+c = [4970,1191.22] 
+python = [50816.8,555.9] 
+
+
   
 X_axis = np.arange(len(X)) 
   
-plt.bar(X_axis - 0.2, Ygirls, 0.4, label = 'C') 
-plt.bar(X_axis + 0.2, Zboys, 0.4, label = 'Python') 
+plt.bar(X_axis - 0.2, c, 0.4, label = 'C') 
+plt.bar(X_axis + 0.2, python, 0.4, label = 'Python') 
   
 plt.xticks(X_axis, X) 
 #plt.xlabel("Tempi per le varie fasi dell algoritmo") 
@@ -17,5 +23,3 @@ plt.ylabel("Millisecondi")
 plt.title("Media e deviazione di un istanza completa") 
 plt.legend() 
 plt.show()
-
-# a 512 bit
